c80beb62b84a74b678f55722de24eb041f6da83d
shubh425 <115350950+shubh425@users.noreply.github.com>
2023-04-24 14:04:13 +0530

    Add files via upload

--- CronExpressionParser.py
+++ CronExpressionParser.py
@@ -1,3 +1,5 @@
+# Allowed special characters are asterisk(*), comma(,), dash(-), slash(/)
+
 import re
 COLUMN_SIZE = 14
 
@@ -8,30 +10,33 @@ labels = {"minute": "minute", "hour": "hour", "dayOfMonth": "day of month", "mon
 field_map = {
     "minute": {"label": "minute", "range": [0, 59]},
     "hour": {"label": "hour", "range": [0, 23]},
-    "dayOfMonth": {"label": "day of month", "range": [1, 31]},
+    "dayOfMonth": {"label": "day of month", "range": [1,31]},
     "month": {"label": "month", "range": [1, 12]},
     "dayOfWeek": {"label": "day of week", "range": [1, 7]}
 }
 
 def number_parser(field, value):                                    #function to check whether input is in standard range
-    if not value.isnumeric():
-        return None
-    value = int(value)
-    low, high = field_map[field]["range"]
-    if value < low or value > high:
-        raise IndexError("Invalid range.")
-    return str(value)
+    if value.isnumeric():            
+        value = int(value)
+        low, high = field_map[field]["range"]                       #list unpacking
+        if value < low or value > high:                             #if value not in range, raise error
+            raise IndexError("Invalid range.")
+        return str(value)
 
 
 def comma_parser(field, value, separator = " "):                    #function to check if there are comma separated values
     if re.match(r"^[0-9]+(,[0-9]+)*$", value):
         parts = value.split(",")
+        low, high = field_map[field]["range"]
+        for num in parts:
+            if int(num) < low or int(num) > high:
+                raise IndexError("Invalid range.")
         return separator.join(parts)
 
 
 def range_parser(field, value):                                     #function to create the range if values separated by '-' 
     if re.match(r"^[0-9]+-[0-9]+$", value):
-        low, high = map(int, value.split("-"))
+        low, high = map(int, value.split("-"))                      #if range already provided in cron string
         return expand(low, high, field_map[field]["range"])
 
 
@@ -59,43 +64,45 @@ parsers = [
 ]
 
 
-def expand(low, high, range=[], step=1):
+def expand(low, high, range=[], step=1):                            #function to expand the range... to be used in range_parser,star_parser,step_parser
     low = int(low)
     high = int(high)
     step = int(step)
-    if low > high:
-        return []
     min, max = range
+    if low < min or low > high or high > max:
+        raise IndexError("Invalid range.")                                                
     result = []
-    current = low
-    while current <= high:
-        if (min is not None and current < min) or (max is not None and current > max):
-            raise IndexError("Invalid range.")
-        result.append(current)
-        current += step
+    while low <= high:
+        result.append(low)
+        low += step
     return " ".join(map(str,result))
 
 
-def formatted_output(parsed, padding=COLUMN_SIZE):
+def formatted_output(parsed, padding=COLUMN_SIZE):                  #function to format the output as per our need
     output = []
     for field in fields:
-        output.append(f"{labels[field].ljust(padding)} {parsed[field]}")
+        output.append(f"{labels[field].ljust(padding)}{parsed[field]}")
     return "\n".join(output)
 
 
-def parse(cron):
+def parse(cron):                                                    #main function to parse through the cron expression
     if not isinstance(cron, str):
         raise TypeError("Expected a string")
     cron = cron.strip()
     parts = cron.split(" ")
     if len(parts) != 6:
         raise TypeError("Invalid cron format")
+
+    for char in ''.join(parts[:5]):                                  #this block needs to be added in original code
+        if char.isalpha() or char in ("~!#$%^&()_?."):
+            raise ValueError("Invalid cron format")
+    
     command = parts[5]
     parsed = {}
     for idx, field in enumerate(fields):
-        value = command if field == "command" else "-"
+        value = ""
         if field == "command":
-            parsed[field] = value
+            parsed[field] = command
             continue
         for parser in parsers:
             result = parser(field, parts[idx])
@@ -104,5 +111,3 @@ def parse(cron):
                 break
         parsed[field] = value
     return formatted_output(parsed)
-
-print(parse('*/15 0 1,15 * 1-5 /usr/bin/find'))
--- test_cases.py
+++ test_cases.py
@@ -11,6 +11,7 @@ class TestParse(unittest.TestCase):
 
     def test_invalid_format(self):
         self.assertRaises(TypeError, parse, "1 2 3 4 5")
+        self.assertRaises(ValueError, parse, "a * 1 2 4 /usr/bin/foo")
 
     def test_invalid_range(self):
         self.assertRaises(IndexError, parse, "60 * * * * /usr/bin/foo")
@@ -43,15 +44,24 @@ class TestParse(unittest.TestCase):
     def test_comma_parser(self):
         self.assertEqual(comma_parser("minute", "1,2,3"), "1 2 3")
         self.assertEqual(comma_parser("hour", "0,12,23"), "0 12 23")
+        self.assertRaises(IndexError,comma_parser, "minute", "60,3,1")
+        self.assertRaises(IndexError,comma_parser, "dayOfWeek", "0,1,5")
 
     def test_range_parser(self):
         self.assertEqual(range_parser("minute", "0-30"), "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30")
         self.assertEqual(range_parser("hour", "9-17"), "9 10 11 12 13 14 15 16 17")
         self.assertRaises(IndexError, range_parser, "dayOfMonth", "1-32")
+        self.assertRaises(IndexError, range_parser, "dayOfMonth", "10-5")
 
     def test_star_parser(self):
         self.assertEqual(star_parser("minute", "*"), "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59")
         self.assertEqual(star_parser("hour", "*"), "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23")
+
+    def test_step_parser(self):
+        self.assertEqual(step_parser("minute", "*/5"), "0 5 10 15 20 25 30 35 40 45 50 55")
+        self.assertEqual(step_parser("minute", "5/10"), "5 15 25 35 45 55")
+        self.assertRaises(IndexError, step_parser, "minute", "60/5")
+        self.assertRaises(IndexError, step_parser, "dayOfWeek", "0/2")
     
 if __name__ == '__main__':
     unittest.main()
